eee718c929ebf163a3d4b0a5c454fa7db1571bd4
Anusha Saad <62885127+anushaasaad@users.noreply.github.com>
2021-06-24 16:32:36 +0500

    Create contact.js

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './profile.css';
+
+export default function contact() {
+    return (
+        <div className="content">
+            <ul>
+                <li>Linkedin: <a href="https://www.linkedin.com/in/anusha-saad-444a891a3/">https://www.linkedin.com/anushasaad</a></li>
+                <li>Github: <a href="https://github.com/anushaasaad">https://github.com/anushaasaad</a></li>
+            </ul>
+        </div>
+    )
+}
